51744be83ae280b2dd72dd708732afa39eece05b
reflying <278432993@qq.com>
2019-05-16 15:04:22 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# kafka
+# 
 
-#启动方法
+# kafka启动方法
 
 cd  kafka
+docker-compose up -d
+
+
+# rabbitmq启动方法
+
+cd  rabbitmq
 docker-compose up -d
---
+++ docker_install.sh
@@ -0,0 +1,24 @@
+#
+
+# 环境:centos7
+# docker 安装
+sudo  yum install -y yum-utils device-mapper-persistent-data lvm2 git
+sudo  yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+sudo  yum makecache fast
+sudo  yum -y install docker-ce
+
+# 添加阿里云镜像加速
+sudo mkdir -p /etc/docker
+
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://1jqsv485.mirror.aliyuncs.com"]
+}
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo systemctl enable docker.service
+
+# 安装docker-compose
+pip install  docker-compose
